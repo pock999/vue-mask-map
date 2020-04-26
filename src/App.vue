6831453a99278b61c215a13f62ecfbde7624b4e6
rick <rickhou44@gmai.com>
2020-04-26 17:01:49 +0800

    left side list can be updated, and add some comment

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,6 @@
 <template>
   <div id="app">
+    <!-- loading組件 -->
     <loading loader="bars"
         :active.sync="isLoading"
         :can-cancel="true"
@@ -8,12 +9,14 @@
       <div class="row">
         <div class="col-md-4">
           <div class="form-group">
+            <!-- 線是選單 -->
             <select class="form-control form-control-lg" v-model="county">
               <option value=''>--請選擇縣市--</option>
               <option v-for="(item, index) in countyList" :key="index" :value="item">
                 {{ item }}
               </option>
             </select>
+            <!-- 鄉鎮市區選單 -->
             <select class="form-control mt-2" v-model="town">
               <option value=''>--請選擇行政區--</option>
               <option v-for="(item, index) in townList" :key="index" :value="item">
@@ -21,6 +24,7 @@
               </option>
             </select>
           </div>
+          <!-- 左側清單 -->
           <div class="list-group d-none d-md-block">
             <a class="list-group-item list-group-item-action flex-column
             align-items-start" v-for="(item, index) in showList" :key="index">
@@ -42,8 +46,12 @@
           </div>
         </div>
         <div class="col-md-8 map-container">
+          <!-- 將apiData(api獲得的資料)傳入子組件的maskData，讓子組件整理成可用格式
+              將penToLocation(地圖定位座標)傳入子組件，使其在地圖上定位
+              updateShowList 更新 showList方法，由子組件發起
+              initList 更新 api整理後資料，由子組件發起 -->
           <MapView :mask-data = "apiData" @updateShowList="updateShowList"
-          :pento-locate = "penToLocation"/>
+          :pento-locate = "penToLocation" @initList="initList"/>
         </div>
       </div>
     </div>
@@ -55,8 +63,6 @@
 import VueLoading from 'vue-loading-overlay';
 import MapView from './components/MapView.vue';
 
-const BreakException = {};
-
 export default {
   name: 'App',
   components: {
@@ -66,42 +72,63 @@ export default {
   data() {
     return {
       apiURL: 'https://raw.githubusercontent.com/kiang/pharmacies/master/json/points.json',
+      /* api獲得資料，不能直接使用(未整理) */
       apiData: [],
+      /* api整理後的資料，可以直接使用，和apiData差別在這個可以直接使用 */
+      initData: [],
+      /* 顯示在左側清單上的資料，隨著所選縣市鄉鎮更改 */
       showList: [],
-      countyList: ['基隆市', '臺北市', '新北市', '連江縣', '宜蘭縣', '釣魚臺', '新竹市', '新竹縣', '桃園市', '苗栗縣', '臺中市', '彰化縣', '南投縣', '嘉義市', '嘉義縣', '雲林縣', '臺南市', '高雄市', '南海島', '澎湖縣', '金門縣', '屏東縣', '臺東縣', '花蓮縣'],
+      /* 縣市列表 */
+      countyList: ['基隆市', '臺北市', '新北市', '連江縣', '宜蘭縣', '新竹市', '新竹縣', '桃園市', '苗栗縣', '臺中市', '彰化縣', '南投縣', '嘉義市', '嘉義縣', '雲林縣', '臺南市', '高雄市', '南海島', '澎湖縣', '金門縣', '屏東縣', '臺東縣', '花蓮縣'],
+      /* 鄉鎮市區列表 */
       townList: [],
+      /* 表示下拉式所選之縣市值 */
       county: '',
+      /* 表示下拉式所選之鄉鎮市區值 */
       town: '',
+      /* 地圖定位座標 */
       penToLocation: [],
+      /* 控制loading組件是否顯示 */
       isLoading: true,
     };
   },
   watch: {
+    /* 監看選擇下拉式縣市值，若有改變，則在所有資料裡面撈出該縣市之所有資料的鄉鎮市區
+        ，並去除重複更新至鄉鎮市區列表
+    */
     county(val) {
       const ls = [];
-      this.showList.forEach((item) => {
+      this.initData.forEach((item) => {
         if (item.properties.county === val) {
           ls.push(item.properties.town);
         }
       });
       this.townList = [...new Set(ls)];
     },
+    /* 監看選擇下拉式鄉鎮市區值，若有改變，則在所有資料裡面撈出該縣市、鄉鎮市區的所有資料
+        ，並更新至showList(顯示在左側清單上的資料，隨著所選縣市鄉鎮更改)
+    */
     town(val) {
       console.log(val);
+      const newShowList = [];
       try {
-        this.showList.forEach((item) => {
+        this.initData.forEach((item) => {
           if (item.properties.town === val) {
             this.penToLocation = [item.geometry.coordinates[1], item.geometry.coordinates[0]];
             console.log(this.penToLocation);
-            throw BreakException;
+          }
+          if (item.properties.address.includes(this.county) && item.properties.town === val) {
+            newShowList.push(item);
           }
         });
+        this.updateShowList(newShowList);
       } catch (e) {
-        console.log('find');
+        console.log('錯誤');
       }
     },
   },
   methods: {
+    /* 請求API */
     loadingAPI() {
       this.$http.get(this.apiURL).then((response) => {
         // console.log(response.data);
@@ -109,17 +136,22 @@ export default {
       }).then(() => {
         setTimeout(() => {
           this.isLoading = false;
-        }, 2000);
+        }, 5000);
       }).catch((e) => {
         console.log(e);
         alert('不好意思ajax出了問題!!!');
       });
     },
+    /* 更新showList */
     updateShowList(showData) {
       this.showList = showData;
     },
+    /* 更新initData(api整理後的資料) */
+    initList(showData) {
+      this.initData = showData;
+    },
   },
-  beforeMount() {
+  created() {
     this.loadingAPI();
   },
 };
--- src/components/MapView.vue
+++ src/components/MapView.vue
@@ -8,6 +8,7 @@ import 'leaflet.markercluster';
 
 let map;
 const markerLayer = L.markerClusterGroup();
+/* 紅色icon */
 const redIcon = new L.Icon({
   iconUrl: 'https://cdn.rawgit.com/pointhi/leaflet-color-markers/master/img/marker-icon-2x-red.png',
   shadowUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/0.7.7/images/marker-shadow.png',
@@ -17,6 +18,7 @@ const redIcon = new L.Icon({
   shadowSize: [41, 41],
 });
 
+/* 藍色icon */
 const blueIcon = new L.Icon({
   iconUrl: 'https://cdn.rawgit.com/pointhi/leaflet-color-markers/master/img/marker-icon-2x-blue.png',
   shadowUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/0.7.7/images/marker-shadow.png',
@@ -25,7 +27,7 @@ const blueIcon = new L.Icon({
   popupAnchor: [1, -34],
   shadowSize: [41, 41],
 });
-
+/* 灰色icon */
 const greyIcon = new L.Icon({
   iconUrl: 'https://cdn.rawgit.com/pointhi/leaflet-color-markers/master/img/marker-icon-2x-grey.png',
   shadowUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/0.7.7/images/marker-shadow.png',
@@ -40,22 +42,31 @@ export default {
   props: ['maskData', 'pentoLocate'],
   data() {
     return {
+      /* 起始中心位置 */
       centerCoord: [],
       parseData: [],
       showData: [],
     };
   },
   methods: {
+    /* 呼叫父組件更新 showList方法 */
     updateListData() {
       this.$emit('updateShowList', this.showData);
     },
+    /* 呼叫父組件更新 api整理後資料 */
+    initData() {
+      this.$emit('initList', this.showData);
+    },
+    /* 使用GPS定位功能 */
     getLocation() {
       if (navigator.geolocation) {
         navigator.geolocation.getCurrentPosition(this.showPosition, this.showError);
       } else {
-        alert('出了一點小狀況，請稍後再試');
+        this.showError();
+        alert('定位可能出了一點小狀況');
       }
     },
+    /* 若使用者同意定位，則初始化地圖，並將地圖focus至使用者的位置 */
     showPosition(position) {
     //   const latlon = `${position.coords.latitude},${position.coords.longitude}`;
       map = L.map('map').setView([position.coords.latitude, position.coords.longitude], 10);
@@ -64,6 +75,7 @@ export default {
       }).addTo(map);
       this.centerCoord = [position.coords.latitude, position.coords.longitude];
     },
+    /* 若使用者不同意定位，則初始化地圖，並將地圖focus至預設定義的位置 */
     showError() {
       this.centerCoord = [23.635915, 121.063619];
       map = L.map('map').setView([23.635915, 120.863619], 10);
@@ -71,45 +83,56 @@ export default {
         attribution: '<a href="https://www.openstreetmap.org/">OSM</a>',
       }).addTo(map);
     },
-    importDate() {
+    /* 將資料放入地圖 */
+    importData() {
+      /* 將api傳入資料變成js可用的形式 */
       this.maskData.forEach((element) => {
         this.parseData.push(JSON.parse(JSON.stringify(element)));
       });
+      /* 將資料放入地圖 */
       this.parseData.forEach((ele) => {
         const thisLocat = ele.geometry.coordinates;
         let thisMarker;
         if (ele.properties.mask_adult === 0 && ele.properties.mask_child === 0) {
+          // 成人口罩數量=0 且 兒童口罩數量=0 ==> 給予灰色圖標
           thisMarker = L.marker([thisLocat[1], thisLocat[0]], { icon: greyIcon });
         } else if (ele.properties.mask_adult < 10 && ele.properties.mask_child < 10) {
+          // 成人口罩數量<10 且 兒童口罩數量<10 ==> 給予紅色圖標
           thisMarker = L.marker([thisLocat[1], thisLocat[0]], { icon: redIcon });
         } else {
           thisMarker = L.marker([thisLocat[1], thisLocat[0]], { icon: blueIcon });
         }
+        /* 給予圖標點擊後跑出的小視窗要顯示的內容 */
         thisMarker.bindPopup(`<h2>${ele.properties.name}</h2><h4>大人口罩:${ele.properties.mask_adult}</h4><h4>小孩口罩${ele.properties.mask_child}</h4>`).openPopup();
         markerLayer.addLayer(thisMarker);
       });
       map.addLayer(markerLayer);
+      /* 更新資料 */
       this.showData = this.parseData;
       this.updateListData();
     },
     mapPanTo(newLocate) {
+      /* 定位至傳入座標 */
       map.panTo(newLocate);
     },
   },
   beforeMount() {
     this.getLocation();
     if (typeof this.maskData[0] !== 'undefined') {
-      //    若有資料，就放入地圖
-      this.importDate();
+      //    若有資料，就放入地圖，並更新初始資料
+      this.importData();
+      this.initData();
     }
   },
   watch: {
+    // 當api資料更新，放入地圖，並更新初始資料
     maskData(newVal) {
-      // 資料更新，放入地圖
       if (typeof newVal[0] !== 'undefined') {
-        this.importDate();
+        this.importData();
+        this.initData();
       }
     },
+    /* 當該資料有變動，表示需要定位至新座標 */
     pentoLocate(newLocate) {
       this.mapPanTo(newLocate);
     },
